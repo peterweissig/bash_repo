c7436de0dfd74777252cef5dd9dfcab17137301e
Peter Weissig <peterweissig@github.com>
2018-02-09 01:01:21 +0100

    updated repos according to new repo structure   + moved some repos around (e.g. bash_roboag from peterweissig to roboag)

--- alias.sh
+++ alias.sh
@@ -1,31 +1,31 @@
 #***************************[bash]*******************************************
-# 2018 01 11
+# 2018 02 08
 
-# repositories (github)
+# repositories (peter)
 alias git_clone_bash_repo="_repo_git_clone ${REPO_BASH_REPO[*]}"
 alias git_pull_bash_repo="_repo_git_pull \"${REPO_BASH_REPO[0]}\""
 alias git_push_bash_repo="_repo_git_push \"${REPO_BASH_REPO[0]}\""
 alias git_status_bash_repo="_repo_git_st \"${REPO_BASH_REPO[0]}\""
 
-# config functions (github)
+# config functions (peter)
 alias git_clone_bash_config="_repo_git_clone ${REPO_BASH_CONFIG[*]}"
 alias git_pull_bash_config="_repo_git_pull \"${REPO_BASH_CONFIG[0]}\""
 alias git_push_bash_config="_repo_git_push \"${REPO_BASH_CONFIG[0]}\""
 alias git_status_bash_config="_repo_git_st \"${REPO_BASH_CONFIG[0]}\""
 
-# network functions (github)
+# network functions (peter)
 alias git_clone_bash_network="_repo_git_clone ${REPO_BASH_NETWORK[*]}"
 alias git_pull_bash_network="_repo_git_pull \"${REPO_BASH_NETWORK[0]}\""
 alias git_push_bash_network="_repo_git_push \"${REPO_BASH_NETWORK[0]}\""
 alias git_status_bash_network="_repo_git_st \"${REPO_BASH_NETWORK[0]}\""
 
-# multimedia (github)
+# multimedia (peter)
 alias git_clone_bash_multimedia="_repo_git_clone ${REPO_BASH_MULTIMEDIA[*]}"
 alias git_pull_bash_multimedia="_repo_git_pull \"${REPO_BASH_MULTIMEDIA[0]}\""
 alias git_push_bash_multimedia="_repo_git_push \"${REPO_BASH_MULTIMEDIA[0]}\""
 alias git_status_bash_multimedia="_repo_git_st \"${REPO_BASH_MULTIMEDIA[0]}\""
 
-# roboag (github)
+# roboag (roboag)
 alias git_clone_bash_roboag="_repo_git_clone ${REPO_BASH_ROBOAG[*]}"
 alias git_pull_bash_roboag="_repo_git_pull \"${REPO_BASH_ROBOAG[0]}\""
 alias git_push_bash_roboag="_repo_git_push \"${REPO_BASH_ROBOAG[0]}\""
@@ -34,9 +34,9 @@ alias git_status_bash_roboag="_repo_git_st \"${REPO_BASH_ROBOAG[0]}\""
 
 
 #***************************[robo]*******************************************
-# 2018 01 23
+# 2018 02 08
 
-# robolib (github)
+# robolib (roboag)
 alias git_clone_robo_lib="_repo_git_clone ${REPO_ROBO_LIB[*]}"
 alias git_pull_robo_lib="_repo_git_pull \"${REPO_ROBO_LIB[0]}\""
 alias git_push_robo_lib="_repo_git_push \"${REPO_ROBO_LIB[0]}\""
@@ -48,8 +48,9 @@ alias git_pull_robo_pololu="_repo_git_pull \"${REPO_ROBO_POLOLU[0]}\""
 alias git_status_robo_pololu="_repo_git_st \"${REPO_ROBO_POLOLU[0]}\""
 
 #***************************[robo/eagle]
-# 2018 01 30
+# 2018 02 08
 
+# config (roboag)
 alias git_clone_robo_eagle_config="_repo_git_clone \
   ${REPO_ROBO_EAGLE_CONFIG[*]}"
 alias git_pull_robo_eagle_config="_repo_git_pull \
@@ -59,6 +60,7 @@ alias git_push_robo_eagle_config="_repo_git_push \
 alias git_status_robo_eagle_config="_repo_git_st \
   \"${REPO_ROBO_EAGLE_CONFIG[0]}\""
 
+# keplerboard (roboag)
 alias git_clone_robo_eagle_keplerboard="_repo_git_clone \
   ${REPO_ROBO_EAGLE_KEPLERBOARD[*]}"
 alias git_pull_robo_eagle_keplerboard="_repo_git_pull \
@@ -68,6 +70,7 @@ alias git_push_robo_eagle_keplerboard="_repo_git_push \
 alias git_status_robo_eagle_keplerboard="_repo_git_st \
   \"${REPO_ROBO_EAGLE_KEPLERBOARD[0]}\""
 
+# xbee (roboag)
 alias git_clone_robo_eagle_xbee="_repo_git_clone \
   ${REPO_ROBO_EAGLE_XBEE[*]}"
 alias git_pull_robo_eagle_xbee="_repo_git_pull \
@@ -77,6 +80,7 @@ alias git_push_robo_eagle_xbee="_repo_git_push \
 alias git_status_robo_eagle_xbee="_repo_git_st \
   \"${REPO_ROBO_EAGLE_XBEE[0]}\""
 
+# logic (roboag)
 alias git_clone_robo_eagle_logic="_repo_git_clone \
   ${REPO_ROBO_EAGLE_LOGIC[*]}"
 alias git_pull_robo_eagle_logic="_repo_git_pull \
@@ -86,6 +90,7 @@ alias git_push_robo_eagle_logic="_repo_git_push \
 alias git_status_robo_eagle_logic="_repo_git_st \
   \"${REPO_ROBO_EAGLE_LOGIC[0]}\""
 
+# voltage monitor (roboag)
 alias git_clone_robo_eagle_vtgMon="_repo_git_clone \
   ${REPO_ROBO_EAGLE_VTGMON[*]}"
 alias git_pull_robo_eagle_vtgMon="_repo_git_pull \
@@ -96,13 +101,15 @@ alias git_status_robo_eagle_vtgMon="_repo_git_st \
   \"${REPO_ROBO_EAGLE_VTGMON[0]}\""
 
 #***************************[robo/robosax]
-# 2018 01 30
+# 2018 02 08
 
+# led-box (robosax) - original repo is from fabiangeissler
 alias git_clone_robosax_ledbox="_repo_git_clone ${REPO_ROBOSAX_LEDBOX[*]}"
 alias git_pull_robosax_ledbox="_repo_git_pull \"${REPO_ROBOSAX_LEDBOX[0]}\""
 alias git_push_robosax_ledbox="_repo_git_push \"${REPO_ROBOSAX_LEDBOX[0]}\""
 alias git_status_robosax_ledbox="_repo_git_st \"${REPO_ROBOSAX_LEDBOX[0]}\""
 
+# licht-tht software (robosax)
 alias git_clone_robosax_licht_tht="_repo_git_clone \
   ${REPO_ROBOSAX_LICHT_THT[*]}"
 alias git_pull_robosax_licht_tht="_repo_git_pull \
@@ -112,6 +119,7 @@ alias git_push_robosax_licht_tht="_repo_git_push \
 alias git_status_robosax_licht_tht="_repo_git_st \
   \"${REPO_ROBOSAX_LICHT_THT[0]}\""
 
+# licht-tht circuit (robosax)
 alias git_clone_robosax_licht_avr="_repo_git_clone \
   ${REPO_ROBOSAX_LICHT_AVR[*]}"
 alias git_pull_robosax_licht_avr="_repo_git_pull \
@@ -121,14 +129,16 @@ alias git_push_robosax_licht_avr="_repo_git_push \
 alias git_status_robosax_licht_avr="_repo_git_st \
   \"${REPO_ROBOSAX_LICHT_AVR[0]}\""
 
+# punkte (robosax)
 alias git_clone_robosax_points="_repo_git_clone ${REPO_ROBOSAX_POINTS[*]}"
 alias git_pull_robosax_points="_repo_git_pull \"${REPO_ROBOSAX_POINTS[0]}\""
 alias git_push_robosax_points="_repo_git_push \"${REPO_ROBOSAX_POINTS[0]}\""
 alias git_status_robosax_points="_repo_git_st \"${REPO_ROBOSAX_POINTS[0]}\""
 
 #***************************[robo/roboag]
-# 2018 01 30
+# 2018 02 08
 
+# gluecksrad (roboag)
 alias git_clone_roboag_gluecksrad="_repo_git_clone \
   ${REPO_ROBOAG_GLUECKSRAD[*]}"
 alias git_pull_roboag_gluecksrad="_repo_git_pull \
@@ -138,6 +148,7 @@ alias git_push_roboag_gluecksrad="_repo_git_push \
 alias git_status_roboag_gluecksrad="_repo_git_st \
   \"${REPO_ROBOAG_GLUECKSRAD[0]}\""
 
+# 3pi modification (roboag)
 alias git_clone_roboag_3pi="_repo_git_clone ${REPO_ROBOAG_3PI[*]}"
 alias git_pull_roboag_3pi="_repo_git_pull \"${REPO_ROBOAG_3PI[0]}\""
 alias git_push_roboag_3pi="_repo_git_push \"${REPO_ROBOAG_3PI[0]}\""
@@ -148,8 +159,9 @@ alias git_status_roboag_3pi="_repo_git_st \"${REPO_ROBOAG_3PI[0]}\""
 #***************************[projects]****************************************
 
 #***************************[projects/eagle]
-# 2018 01 30
+# 2018 02 08
 
+# dmx-driver (peter)
 alias git_clone_eagle_dmx_driver="_repo_git_clone \
   ${REPO_EAGLE_DMX_DRIVER[*]}"
 alias git_pull_eagle_dmx_driver="_repo_git_pull \
@@ -159,6 +171,7 @@ alias git_push_eagle_dmx_driver="_repo_git_push \
 alias git_status_eagle_dmx_driver="_repo_git_st \
   \"${REPO_EAGLE_DMX_DRIVER[0]}\""
 
+# opa (peter)
 alias git_clone_eagle_dmx_opa="_repo_git_clone \
   ${REPO_EAGLE_DMX_OPA[*]}"
 alias git_pull_eagle_dmx_opa="_repo_git_pull \
@@ -169,8 +182,9 @@ alias git_status_eagle_dmx_opa="_repo_git_st \
   \"${REPO_EAGLE_DMX_OPA[0]}\""
 
 #***************************[projects/hardware]
-# 2018 01 30
+# 2018 02 08
 
+# audio system (peter)
 alias git_clone_hardware_audio_home="_repo_git_clone \
   ${REPO_HARDWARE_AUDIO_HOME[*]}"
 alias git_pull_hardware_audio_home="_repo_git_pull \
@@ -180,6 +194,7 @@ alias git_push_hardware_audio_home="_repo_git_push \
 alias git_status_hardware_audio_home="_repo_git_st \
   \"${REPO_HARDWARE_AUDIO_HOME[0]}\""
 
+# dimmer (peter)
 alias git_clone_hardware_dimmer="_repo_git_clone \
   ${REPO_HARDWARE_DIMMER[*]}"
 alias git_pull_hardware_dimmer="_repo_git_pull \
@@ -190,7 +205,7 @@ alias git_status_hardware_dimmer="_repo_git_st \
   \"${REPO_HARDWARE_DIMMER[0]}\""
 
 #***************************[projects/c++]
-# 2018 01 05
+# 2018 02 08
 
 # all
 alias git_pull_cpp="if [ -d \"${REPO_CPP_MAIN[0]}\" ]; then \
@@ -198,37 +213,37 @@ alias git_pull_cpp="if [ -d \"${REPO_CPP_MAIN[0]}\" ]; then \
 alias git_status_cpp="if [ -d \"${REPO_CPP_MAIN[0]}\" ]; then \
   bash -c 'cd \"${REPO_CPP_MAIN[0]}\" && make status'; fi"
 
-# main (github)
+# main (peter)
 alias git_clone_cpp_main="_repo_git_clone ${REPO_CPP_MAIN[*]}"
 alias git_pull_cpp_main="_repo_git_pull \"${REPO_CPP_MAIN[0]}\""
 alias git_push_cpp_main="_repo_git_push \"${REPO_CPP_MAIN[0]}\""
 alias git_status_cpp_main="_repo_git_st \"${REPO_CPP_MAIN[0]}\""
 
-# basic (github)
+# basic (peter)
 alias git_clone_cpp_basic="_repo_git_clone ${REPO_CPP_BASIC[*]}"
 alias git_pull_cpp_basic="_repo_git_pull \"${REPO_CPP_BASIC[0]}\""
 alias git_push_cpp_basic="_repo_git_push \"${REPO_CPP_BASIC[0]}\""
 alias git_status_cpp_basic="_repo_git_st \"${REPO_CPP_BASIC[0]}\""
 
-# comport (github)
+# comport (peter)
 alias git_clone_cpp_comport="_repo_git_clone ${REPO_CPP_COMPORT[*]}"
 alias git_pull_cpp_comport="_repo_git_pull \"${REPO_CPP_COMPORT[0]}\""
 alias git_push_cpp_comport="_repo_git_push \"${REPO_CPP_COMPORT[0]}\""
 alias git_status_cpp_comport="_repo_git_st \"${REPO_CPP_COMPORT[0]}\""
 
-# file (github)
+# file (peter)
 alias git_clone_cpp_file="_repo_git_clone ${REPO_CPP_FILE[*]}"
 alias git_pull_cpp_file="_repo_git_pull \"${REPO_CPP_FILE[0]}\""
 alias git_push_cpp_file="_repo_git_push \"${REPO_CPP_FILE[0]}\""
 alias git_status_cpp_file="_repo_git_st \"${REPO_CPP_FILE[0]}\""
 
-# avr-downloader (github)
+# avr-downloader (peter)
 alias git_clone_cpp_downloader="_repo_git_clone ${REPO_CPP_DOWNLOADER[*]}"
 alias git_pull_cpp_downloader="_repo_git_pull \"${REPO_CPP_DOWNLOADER[0]}\""
 alias git_push_cpp_downloader="_repo_git_push \"${REPO_CPP_DOWNLOADER[0]}\""
 alias git_status_cpp_downloader="_repo_git_st \"${REPO_CPP_DOWNLOADER[0]}\""
 
-# xbee-tool (github)
+# xbee-tool (peter)
 alias git_clone_cpp_xbee="_repo_git_clone ${REPO_CPP_XBEE[*]}"
 alias git_pull_cpp_xbee="_repo_git_pull \"${REPO_CPP_XBEE[0]}\""
 alias git_push_cpp_xbee="_repo_git_push \"${REPO_CPP_XBEE[0]}\""
--- list.sh
+++ list.sh
@@ -6,100 +6,112 @@
 
 
 #***************************[server]******************************************
-# 2018 02 04
+# 2018 02 08
 
 export REPO_ROOT_GITHUB_PETER="https://github.com/peterweissig/"
 export REPO_ROOT_GITHUB_ROBOAG="https://github.com/roboag/"
+export REPO_ROOT_GITHUB_ROBOSAX="https://github.com/robosax/"
 
 
 
 #***************************[bash]********************************************
-# 2018 01 11
+# 2018 02 08
 
 # paths
 export REPO_BASH_PATH="${REPO_PATH_WORKSPACE}bash/"
 
-# repositories (github)
+# repositories (peter)
 export REPO_BASH_REPO=("${REPO_BASH_PATH}repo/" \
   "${REPO_ROOT_GITHUB_PETER}bash_repo.git")
 
-# config functions (github)
+# config functions (peter)
 export REPO_BASH_CONFIG=("${REPO_BASH_PATH}config/" \
   "${REPO_ROOT_GITHUB_PETER}bash_config.git")
 
-# network functions (github)
+# network functions (peter)
 export REPO_BASH_NETWORK=("${REPO_BASH_PATH}network/" \
   "${REPO_ROOT_GITHUB_PETER}bash_network.git")
 
-# multimedia (github)
+# multimedia (peter)
 export REPO_BASH_MULTIMEDIA=("${REPO_BASH_PATH}multimedia/" \
   "${REPO_ROOT_GITHUB_PETER}bash_multimedia.git")
 
-# roboag (github)
+# roboag (roboag)
 export REPO_BASH_ROBOAG=("${REPO_BASH_PATH}roboag/" \
-  "${REPO_ROOT_GITHUB_PETER}bash_roboag.git")
+  "${REPO_ROOT_GITHUB_ROBOAG}bash_roboag.git")
 
 
 #***************************[robo]********************************************
-# 2018 01 30
+# 2018 02 08
 
 # paths
 export REPO_ROBO_PATH="${REPO_PATH_WORKSPACE}robo/"
 
-# robolib (github)
+# robolib (roboag)
 export REPO_ROBO_LIB=("${REPO_ROBO_PATH}library/" \
-  "${REPO_ROOT_GITHUB_PETER}robolib.git")
+  "${REPO_ROOT_GITHUB_ROBOAG}avr_robolib.git")
 
 # pololu (extern)
 export REPO_ROBO_POLOLU=("${REPO_ROBO_PATH}pololu/" \
   "https://github.com/pololu/libpololu-avr.git")
 
 #***************************[robo/eagle]
-# 2018 01 30
+# 2018 02 08
 
 
 export REPO_ROBO_EAGLE_PATH="${REPO_ROBO_PATH}eagle/"
 
+# config (roboag)
 export REPO_ROBO_EAGLE_CONFIG=("${REPO_ROBO_EAGLE_PATH}config/" \
-  "${REPO_ROOT_GITHUB_PETER}eagle_config.git")
+  "${REPO_ROOT_GITHUB_ROBOAG}eagle_config.git")
 
+# keplerboard (roboag)
 export REPO_ROBO_EAGLE_KEPLERBOARD=("${REPO_ROBO_EAGLE_PATH}keplerboard/" \
-  "${REPO_ROOT_GITHUB_PETER}eagle_keplerboard.git")
+  "${REPO_ROOT_GITHUB_ROBOAG}eagle_keplerboard.git")
 
+# xbee (roboag)
 export REPO_ROBO_EAGLE_XBEE=("${REPO_ROBO_EAGLE_PATH}xbee/" \
-  "${REPO_ROOT_GITHUB_PETER}eagle_xbee.git")
+  "${REPO_ROOT_GITHUB_ROBOAG}eagle_xbee.git")
 
+# logic (roboag)
 export REPO_ROBO_EAGLE_LOGIC=("${REPO_ROBO_EAGLE_PATH}logic/" \
-  "${REPO_ROOT_GITHUB_PETER}eagle_logic.git")
+  "${REPO_ROOT_GITHUB_ROBOAG}eagle_logic.git")
 
+# voltage monitor (roboag)
 export REPO_ROBO_EAGLE_VTGMON=("${REPO_ROBO_EAGLE_PATH}vtgMon/" \
-  "${REPO_ROOT_GITHUB_PETER}eagle_voltageMonitoring.git")
+  "${REPO_ROOT_GITHUB_ROBOAG}eagle_voltageMonitoring.git")
 
 #***************************[robo/robosax]
-# 2018 01 30
+# 2018 02 08
 
 export REPO_ROBOSAX_PATH="${REPO_ROBO_PATH}robosax/"
 
+# led-box (robosax) - original repo is from fabiangeissler
 export REPO_ROBOSAX_LEDBOX=("${REPO_ROBOSAX_PATH}ledbox/" \
-  "https://github.com/fabiangeissler/robosax-ledbox.git")
+  "${REPO_ROOT_GITHUB_ROBOSAX}projekt_ledbox.git")
 
+# licht-tht software (robosax)
 export REPO_ROBOSAX_LICHT_THT=("${REPO_ROBOSAX_PATH}licht_tht/" \
-  "${REPO_ROOT_GITHUB_PETER}eagle_robosax_licht.git")
+  "${REPO_ROOT_GITHUB_ROBOSAX}eagle_licht.git")
 
+# licht-tht circuit (robosax)
 export REPO_ROBOSAX_LICHT_AVR=("${REPO_ROBOSAX_PATH}licht_avr/" \
-  "${REPO_ROOT_GITHUB_PETER}avr_robosax_licht.git")
+  "${REPO_ROOT_GITHUB_ROBOSAX}avr_licht.git")
 
+# punkte (robosax)
 export REPO_ROBOSAX_POINTS=("${REPO_ROBOSAX_PATH}punkte/" \
-  "${REPO_ROOT_GITHUB_PETER}php_robosax_punkte")
+  "${REPO_ROOT_GITHUB_ROBOSAX}php_punkte")
 
 #***************************[robo/roboag]
-# 2018 02 04
+# 2018 02 08
 
 export REPO_ROBOAG_PATH="${REPO_ROBO_PATH}roboag/"
 
+# gluecksrad (roboag)
 export REPO_ROBOAG_GLUECKSRAD=("${REPO_ROBOAG_PATH}gluecksrad/" \
   "${REPO_ROOT_GITHUB_ROBOAG}projekt_gluecksrad.git")
 
+# 3pi modification (roboag)
 export REPO_ROBOAG_3PI=("${REPO_ROBOAG_PATH}3pi/" \
   "${REPO_ROOT_GITHUB_ROBOAG}projekt_3pi.git")
 
@@ -110,55 +122,59 @@ export REPO_ROBOAG_3PI=("${REPO_ROBOAG_PATH}3pi/" \
 export REPO_PROJECTS_PATH="${REPO_PATH_WORKSPACE}projects/"
 
 #***************************[projects/eagle]
-# 2018 01 30
+# 2018 02 08
 
 export REPO_PROJECTS_EAGLE_PATH="${REPO_PROJECTS_PATH}eagle/"
 
+# dmx-driver (peter)
 export REPO_EAGLE_DMX_DRIVER=("${REPO_PROJECTS_EAGLE_PATH}dmx_driver/" \
   "${REPO_ROOT_GITHUB_PETER}eagle_dmx_driver.git")
 
+# opa (peter)
 export REPO_EAGLE_DMX_OPA=("${REPO_PROJECTS_EAGLE_PATH}dmx_opa/" \
   "${REPO_ROOT_GITHUB_PETER}eagle_dmx_opa.git")
 
 #***************************[projects/hardware]
-# 2018 01 30
+# 2018 02 08
 
 export REPO_HARDWARE_PATH="${REPO_PROJECTS_PATH}hardware/"
 
+# audio system (peter)
 export REPO_HARDWARE_AUDIO_HOME=("${REPO_HARDWARE_PATH}audio_home/" \
-  "${REPO_ROOT_GITHUB_PETER}proj_audio_home.git")
+  "${REPO_ROOT_GITHUB_PETER}project_audio_home.git")
 
+# dimmer (peter)
 export REPO_HARDWARE_DIMMER=("${REPO_HARDWARE_PATH}dimmer/" \
   "${REPO_ROOT_GITHUB_PETER}project_dimmer.git")
 
 #***************************[projects/c++]
-# 2018 01 30
+# 2018 02 08
 
 # paths
 export REPO_CPP_PATH="${REPO_PROJECTS_PATH}c++/"
 export REPO_CPP_SRC_PATH="${REPO_CPP_PATH}src/"
 
-# main (github)
+# main (peter)
 export REPO_CPP_MAIN=("${REPO_CPP_PATH}" \
   "${REPO_ROOT_GITHUB_PETER}cpp_main.git")
 
-# basic (github)
+# basic (peter)
 export REPO_CPP_BASIC=("${REPO_CPP_SRC_PATH}basic/" \
   "${REPO_ROOT_GITHUB_PETER}cpp_basic.git")
 
-# comport (github)
+# comport (peter)
 export REPO_CPP_COMPORT=("${REPO_CPP_SRC_PATH}comport/" \
   "${REPO_ROOT_GITHUB_PETER}cpp_comport.git")
 
-# file (github)
+# file (peter)
 export REPO_CPP_FILE=("${REPO_CPP_SRC_PATH}file/" \
   "${REPO_ROOT_GITHUB_PETER}cpp_file.git")
 
-# avr-downloader (github)
+# avr-downloader (peter)
 export REPO_CPP_DOWNLOADER=("${REPO_CPP_SRC_PATH}avr_downloader/" \
   "${REPO_ROOT_GITHUB_PETER}cpp_avr_downloader.git")
 
-# xbee-tool (github)
+# xbee-tool (peter)
 export REPO_CPP_XBEE=("${REPO_CPP_SRC_PATH}xbee_config/" \
   "${REPO_ROOT_GITHUB_PETER}cpp_xbee_config.git")
 
